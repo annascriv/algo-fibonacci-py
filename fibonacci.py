9472bf22d585efc66079fedc25fd32e68d7f896a
annascriv <scriven.anna@outlook.com>
2023-10-05 21:02:24 -0500

    python version of fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,17 @@
 def fibonacci(n):
-  pass
+  fibs = [0,1]
+  i=0
+
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    for x in range(n):
+      fibs.append(fibs[i-2]+fibs[i-1])
+  print(fibs)
+  return   fibs[n]
+
+print(fibonacci(11))
 
 
